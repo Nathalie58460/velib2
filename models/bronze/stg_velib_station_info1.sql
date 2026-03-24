602f45370d0fdddf2ec8cd1a91cb5116ecd67ac1
nathalie lamblin <nathalie.lamblin@talan.com>
2026-03-24 12:53:52 +0000

    mardi 14h

--- models/bronze/stg_velib_station_info1.sql
+++ models/bronze/stg_velib_station_info1.sql
@@ -1,9 +1,25 @@
-SELECT
-    f.value:station_id::number      AS station_id,
-    f.value:name::string            AS name,
-    f.value:lat::float              AS latitude,
-    f.value:lon::float              AS longitude,
-    f.value:capacity::number        AS capacity
-FROM @VELIB_STAGE/station_information.json
-    (file_format => JSON_VELIB),
-    LATERAL FLATTEN(input => $1:data:stations) f
+{{ config(
+    materialized = 'table',
+    tags = ['velib', 'staging']
+) }}
+
+WITH src AS (
+    SELECT
+        PAYLOAD
+    FROM {{ source('velib', 'station_information_raw') }}
+),
+
+stations AS (
+    SELECT
+        f.value:station_id::number      AS station_id,
+        f.value:name::string            AS station_name,
+        f.value:lat::float              AS latitude,
+        f.value:lon::float              AS longitude,
+        f.value:capacity::number        AS capacity,
+        CURRENT_TIMESTAMP()             AS load_timestamp
+    FROM src,
+        LATERAL FLATTEN(input => src.PAYLOAD:data:stations) f
+)
+
+SELECT *
+FROM stations;
